98ac3fe0746b5310937f94766055c161ec7d0d40
comdinet <comdinet@gmail.com>
2025-06-09 19:28:57 +0300

    Add files via upload

--- screen_manager.cpp
+++ screen_manager.cpp
@@ -10,11 +10,11 @@ void ScreenManager::init() {
     instance = this;
     
     screenContainer = lv_obj_create(NULL);
-    lv_obj_set_style_bg_color(screenContainer, lv_color_black(), 0);  // Force black background
+    lv_obj_set_style_bg_color(screenContainer, lv_color_black(), 0);
     lv_obj_set_style_bg_opa(screenContainer, LV_OPA_COVER, 0);
     
     createAllScreens();
-    // setupGestureHandling is now called in createAllScreens()
+    setupGestureHandling();
     
     lv_scr_load(screenContainer);
     showScreen(SCREEN_LIGHT);
@@ -32,9 +32,6 @@ void ScreenManager::createAllScreens() {
     
     screensCreated = true;
     
-    // Set up gestures after all screens are created
-    setupGestureHandling();
-    
     Serial.println("All screens created with gesture handling");
 }
 
@@ -46,34 +43,38 @@ void ScreenManager::createLightScreen() {
     lv_obj_set_style_pad_all(screens[SCREEN_LIGHT], 0, 0);
     lv_obj_clear_flag(screens[SCREEN_LIGHT], LV_OBJ_FLAG_SCROLLABLE);
     
-    // Title - HUGE and centered
-    lv_obj_t* title = createLabel(screens[SCREEN_LIGHT], "LIGHT", 268, 20);
-    lv_obj_set_style_text_color(title, lv_color_white(), 0);
-    lv_obj_set_style_text_font(title, &lv_font_montserrat_22, 0);
+    // Create HUGE "B" using multiple labels to form ASCII art
+    createLabel(screens[SCREEN_LIGHT], "BBB", 20, 40);
+    createLabel(screens[SCREEN_LIGHT], "B  B", 20, 55);
+    createLabel(screens[SCREEN_LIGHT], "BBB", 20, 70);
+    createLabel(screens[SCREEN_LIGHT], "B  B", 20, 85);
+    createLabel(screens[SCREEN_LIGHT], "BBB", 20, 100);
     
-    // Power button - MUCH BIGGER, left side
-    lightElements.powerButton = createButton(screens[SCREEN_LIGHT], "OFF", 30, 70, 120, 60);
-    lv_obj_set_style_bg_color(lightElements.powerButton, lv_color_hex(COLOR_ERROR), 0);
-    lv_obj_set_style_text_font(lv_obj_get_child(lightElements.powerButton, 0), &lv_font_montserrat_22, 0);
-    lv_obj_add_event_cb(lightElements.powerButton, lightPowerButtonEvent, LV_EVENT_CLICKED, this);
+    // Long brightness bar
+    lightElements.brightnessBar = createBar(screens[SCREEN_LIGHT], 90, 50, 430, MIN_BRIGHTNESS, MAX_BRIGHTNESS, 50);
+    lv_obj_add_event_cb(lightElements.brightnessBar, brightnessBarEvent, LV_EVENT_CLICKED, this);
+    lv_obj_add_event_cb(lightElements.brightnessBar, brightnessBarEvent, LV_EVENT_PRESSING, this);
     
-    // Brightness section - BIGGER TEXT, spread across full width
-    lightElements.brightnessLabel = createLabel(screens[SCREEN_LIGHT], "Bright: 50%", 180, 75);
-    lv_obj_set_style_text_color(lightElements.brightnessLabel, lv_color_white(), 0);
-    lv_obj_set_style_text_font(lightElements.brightnessLabel, &lv_font_montserrat_22, 0);
+    // Create HUGE "C" using multiple labels to form ASCII art
+    createLabel(screens[SCREEN_LIGHT], "CCC", 20, 130);
+    createLabel(screens[SCREEN_LIGHT], "C", 20, 145);
+    createLabel(screens[SCREEN_LIGHT], "C", 20, 160);
+    createLabel(screens[SCREEN_LIGHT], "C", 20, 175);
+    createLabel(screens[SCREEN_LIGHT], "CCC", 20, 190);
     
-    // MUCH BIGGER brightness bar - full width
-    lightElements.brightnessBar = createBar(screens[SCREEN_LIGHT], 180, 110, 320, MIN_BRIGHTNESS, MAX_BRIGHTNESS, 50);
+    // Long color temp bar
+    lightElements.colorTempBar = createBar(screens[SCREEN_LIGHT], 90, 140, 430, MIN_COLOR_TEMP, MAX_COLOR_TEMP, 4000);
+    lv_obj_add_event_cb(lightElements.colorTempBar, colorTempBarEvent, LV_EVENT_CLICKED, this);
+    lv_obj_add_event_cb(lightElements.colorTempBar, colorTempBarEvent, LV_EVENT_PRESSING, this);
     
-    // Color temp section - BIGGER TEXT, full width
-    lightElements.colorTempLabel = createLabel(screens[SCREEN_LIGHT], "Color: 4000K", 180, 150);
-    lv_obj_set_style_text_color(lightElements.colorTempLabel, lv_color_white(), 0);
-    lv_obj_set_style_text_font(lightElements.colorTempLabel, &lv_font_montserrat_22, 0);
+    // Create hidden labels for status updates
+    lightElements.brightnessLabel = createLabel(screens[SCREEN_LIGHT], "", 0, 0);
+    lv_obj_add_flag(lightElements.brightnessLabel, LV_OBJ_FLAG_HIDDEN);
+    lightElements.colorTempLabel = createLabel(screens[SCREEN_LIGHT], "", 0, 0);
+    lv_obj_add_flag(lightElements.colorTempLabel, LV_OBJ_FLAG_HIDDEN);
+    lightElements.powerButton = nullptr;
     
-    // MUCH BIGGER color temp bar - full width
-    lightElements.colorTempBar = createBar(screens[SCREEN_LIGHT], 180, 185, 320, MIN_COLOR_TEMP, MAX_COLOR_TEMP, 4000);
-    
-    Serial.println("Light screen - HUGE LAYOUT");
+    Serial.println("Light screen created with HUGE ASCII art B and C");
 }
 
 void ScreenManager::createHVACScreen() {
@@ -85,39 +86,53 @@ void ScreenManager::createHVACScreen() {
     lv_obj_clear_flag(screens[SCREEN_HVAC], LV_OBJ_FLAG_SCROLLABLE);
     lv_obj_add_flag(screens[SCREEN_HVAC], LV_OBJ_FLAG_HIDDEN);
     
-    // Title - HUGE and centered
-    lv_obj_t* title = createLabel(screens[SCREEN_HVAC], "HVAC", 268, 20);
-    lv_obj_set_style_text_color(title, lv_color_white(), 0);
-    lv_obj_set_style_text_font(title, &lv_font_montserrat_22, 0);
-    
-    // Mode buttons - MUCH BIGGER, spread across screen
-    hvacElements.offButton = createButton(screens[SCREEN_HVAC], "OFF", 60, 80, 120, 70);
-    lv_obj_set_style_bg_color(hvacElements.offButton, lv_color_hex(COLOR_SUCCESS), 0);
+    // OFF button at TOP LEFT
+    hvacElements.offButton = createButton(screens[SCREEN_HVAC], "OFF", 150, 10, 100, 50);
+    lv_obj_set_style_bg_color(hvacElements.offButton, lv_color_hex(0x333333), 0);
+    lv_obj_set_style_text_color(lv_obj_get_child(hvacElements.offButton, 0), lv_color_white(), 0);
     lv_obj_set_style_text_font(lv_obj_get_child(hvacElements.offButton, 0), &lv_font_montserrat_22, 0);
     lv_obj_add_event_cb(hvacElements.offButton, hvacOffButtonEvent, LV_EVENT_CLICKED, this);
     
-    hvacElements.coolButton = createButton(screens[SCREEN_HVAC], "COOL", 200, 80, 120, 70);
-    lv_obj_set_style_bg_color(hvacElements.coolButton, lv_color_hex(COLOR_SURFACE), 0);
+    // COOL button at TOP RIGHT
+    hvacElements.coolButton = createButton(screens[SCREEN_HVAC], "COOL", 286, 10, 100, 50);
+    lv_obj_set_style_bg_color(hvacElements.coolButton, lv_color_hex(0x2196F3), 0);
+    lv_obj_set_style_text_color(lv_obj_get_child(hvacElements.coolButton, 0), lv_color_white(), 0);
     lv_obj_set_style_text_font(lv_obj_get_child(hvacElements.coolButton, 0), &lv_font_montserrat_22, 0);
     lv_obj_add_event_cb(hvacElements.coolButton, hvacCoolButtonEvent, LV_EVENT_CLICKED, this);
     
-    // Temperature controls - MUCH BIGGER buttons, spread across bottom
-    hvacElements.tempDownButton = createButton(screens[SCREEN_HVAC], "-", 100, 170, 80, 60);
-    lv_obj_set_style_bg_color(hvacElements.tempDownButton, lv_color_hex(COLOR_SECONDARY), 0);
+    // MINUS button on LEFT SIDE - FULL HEIGHT with BIG minus
+    hvacElements.tempDownButton = createButton(screens[SCREEN_HVAC], "---", 5, 70, 120, 165);
+    lv_obj_set_style_bg_color(hvacElements.tempDownButton, lv_color_hex(0x404040), 0);
+    lv_obj_set_style_text_color(lv_obj_get_child(hvacElements.tempDownButton, 0), lv_color_white(), 0);
     lv_obj_set_style_text_font(lv_obj_get_child(hvacElements.tempDownButton, 0), &lv_font_montserrat_22, 0);
     lv_obj_add_event_cb(hvacElements.tempDownButton, hvacTempDownButtonEvent, LV_EVENT_CLICKED, this);
     
-    // HUGE temperature display
-    hvacElements.targetTempValueLabel = createLabel(screens[SCREEN_HVAC], "22", 268, 180);
-    lv_obj_set_style_text_color(hvacElements.targetTempValueLabel, lv_color_hex(COLOR_PRIMARY), 0);
-    lv_obj_set_style_text_font(hvacElements.targetTempValueLabel, &lv_font_montserrat_22, 0);
-    
-    hvacElements.tempUpButton = createButton(screens[SCREEN_HVAC], "+", 360, 170, 80, 60);
-    lv_obj_set_style_bg_color(hvacElements.tempUpButton, lv_color_hex(COLOR_SECONDARY), 0);
+    // PLUS button on RIGHT SIDE - FULL HEIGHT with BIG plus
+    hvacElements.tempUpButton = createButton(screens[SCREEN_HVAC], "+++", 411, 70, 120, 165);
+    lv_obj_set_style_bg_color(hvacElements.tempUpButton, lv_color_hex(0x404040), 0);
+    lv_obj_set_style_text_color(lv_obj_get_child(hvacElements.tempUpButton, 0), lv_color_white(), 0);
     lv_obj_set_style_text_font(lv_obj_get_child(hvacElements.tempUpButton, 0), &lv_font_montserrat_22, 0);
     lv_obj_add_event_cb(hvacElements.tempUpButton, hvacTempUpButtonEvent, LV_EVENT_CLICKED, this);
     
-    Serial.println("HVAC screen - HUGE LAYOUT");
+    // Create HUGE "22" using ASCII art - First digit "2"
+    createLabel(screens[SCREEN_HVAC], "222", 180, 100);
+    createLabel(screens[SCREEN_HVAC], "  2", 180, 115);
+    createLabel(screens[SCREEN_HVAC], "222", 180, 130);
+    createLabel(screens[SCREEN_HVAC], "2", 180, 145);
+    createLabel(screens[SCREEN_HVAC], "222", 180, 160);
+    
+    // Create HUGE "22" using ASCII art - Second digit "2"
+    createLabel(screens[SCREEN_HVAC], "222", 250, 100);
+    createLabel(screens[SCREEN_HVAC], "  2", 250, 115);
+    createLabel(screens[SCREEN_HVAC], "222", 250, 130);
+    createLabel(screens[SCREEN_HVAC], "2", 250, 145);
+    createLabel(screens[SCREEN_HVAC], "222", 250, 160);
+    
+    // Use one of the labels as reference for updates
+    hvacElements.targetTempValueLabel = createLabel(screens[SCREEN_HVAC], "", 0, 0);
+    lv_obj_add_flag(hvacElements.targetTempValueLabel, LV_OBJ_FLAG_HIDDEN);
+    
+    Serial.println("HVAC screen created with HUGE ASCII art temperature");
 }
 
 lv_obj_t* ScreenManager::createButton(lv_obj_t* parent, const char* text, lv_coord_t x, lv_coord_t y, lv_coord_t w, lv_coord_t h) {
@@ -136,6 +151,8 @@ lv_obj_t* ScreenManager::createLabel(lv_obj_t* parent, const char* text, lv_coor
     lv_obj_t* label = lv_label_create(parent);
     lv_label_set_text(label, text);
     lv_obj_set_pos(label, x, y);
+    lv_obj_set_style_text_color(label, lv_color_white(), 0);
+    lv_obj_set_style_text_font(label, &lv_font_montserrat_22, 0);
     return label;
 }
 
@@ -143,15 +160,15 @@ lv_obj_t* ScreenManager::createBar(lv_obj_t* parent, lv_coord_t x, lv_coord_t y,
     lv_obj_t* bar = lv_bar_create(parent);
     lv_obj_set_pos(bar, x, y);
     lv_obj_set_width(bar, w);
-    lv_obj_set_height(bar, 25);  // MUCH THICKER bars - was 15
+    lv_obj_set_height(bar, 60);
     lv_bar_set_range(bar, min_val, max_val);
     lv_bar_set_value(bar, default_val, LV_ANIM_OFF);
-    lv_obj_set_style_bg_color(bar, lv_color_hex(0x333333), LV_PART_MAIN);  // Dark gray background
-    lv_obj_set_style_bg_color(bar, lv_color_hex(COLOR_PRIMARY), LV_PART_INDICATOR);  // Blue indicator
+    lv_obj_set_style_bg_color(bar, lv_color_hex(0x333333), LV_PART_MAIN);
+    lv_obj_set_style_bg_color(bar, lv_color_hex(0xFF9500), LV_PART_INDICATOR);
+    lv_obj_set_style_border_width(bar, 0, LV_PART_MAIN);
     
-    // Make bars non-clickable to prevent gesture interference
-    lv_obj_clear_flag(bar, LV_OBJ_FLAG_CLICKABLE);
-    lv_obj_add_flag(bar, LV_OBJ_FLAG_GESTURE_BUBBLE);
+    lv_obj_add_flag(bar, LV_OBJ_FLAG_CLICKABLE);
+    lv_obj_clear_flag(bar, LV_OBJ_FLAG_GESTURE_BUBBLE);
     
     return bar;
 }
@@ -193,10 +210,8 @@ void ScreenManager::previousScreen() {
 }
 
 void ScreenManager::setupGestureHandling() {
-    // Set up gesture handling on the screen container
     lv_obj_add_event_cb(screenContainer, gestureEventHandler, LV_EVENT_GESTURE, this);
     
-    // Enable gestures on all screens
     for (int i = 0; i < SCREEN_COUNT; i++) {
         if (screens[i]) {
             lv_obj_add_event_cb(screens[i], gestureEventHandler, LV_EVENT_GESTURE, this);
@@ -211,41 +226,21 @@ void ScreenManager::gestureEventHandler(lv_event_t* e) {
     Serial.print("Gesture detected: ");
     Serial.println(dir);
     
-    // Try both sets of constants to see which work
     if (dir == LV_DIR_TOP) {
         Serial.println("Swipe UP detected - next screen");
         mgr->nextScreen();
     } else if (dir == LV_DIR_BOTTOM) {
         Serial.println("Swipe DOWN detected - previous screen");  
         mgr->previousScreen();
-    } else if (dir == LV_DIR_LEFT) {
-        Serial.println("Swipe LEFT detected - next screen");
-        mgr->nextScreen();
-    } else if (dir == LV_DIR_RIGHT) {
-        Serial.println("Swipe RIGHT detected - previous screen");
-        mgr->previousScreen();
     }
 }
 
 void ScreenManager::updateLightStatus() {
-    if (!lightElements.powerButton) return;
+    if (!lightElements.brightnessBar) return;
     
     if (lightState.available) {
-        // Update power button
-        lv_label_set_text(lv_obj_get_child(lightElements.powerButton, 0), lightState.isOn ? "ON" : "OFF");
-        lv_obj_set_style_bg_color(lightElements.powerButton, lv_color_hex(lightState.isOn ? COLOR_SUCCESS : COLOR_ERROR), 0);
-        
-        // Update brightness - shorter label to prevent overlap
         lv_bar_set_value(lightElements.brightnessBar, lightState.brightness, LV_ANIM_OFF);
-        char brightStr[20];
-        snprintf(brightStr, sizeof(brightStr), "Bright: %d%%", lightState.brightness);
-        lv_label_set_text(lightElements.brightnessLabel, brightStr);
-        
-        // Update color temp - shorter label
         lv_bar_set_value(lightElements.colorTempBar, lightState.colorTemp, LV_ANIM_OFF);
-        char tempStr[20];
-        snprintf(tempStr, sizeof(tempStr), "Color: %dK", lightState.colorTemp);
-        lv_label_set_text(lightElements.colorTempLabel, tempStr);
     }
 }
 
@@ -253,22 +248,20 @@ void ScreenManager::updateHVACStatus() {
     if (!hvacElements.offButton) return;
     
     if (hvacState.available) {
-        // Update button colors based on mode
         if (hvacState.mode == "off" || hvacState.mode == "" || !hvacState.isOn) {
-            lv_obj_set_style_bg_color(hvacElements.offButton, lv_color_hex(COLOR_SUCCESS), 0);
-            lv_obj_set_style_bg_color(hvacElements.coolButton, lv_color_hex(COLOR_SURFACE), 0);
+            lv_obj_set_style_bg_color(hvacElements.offButton, lv_color_hex(0x666666), 0);
+            lv_obj_set_style_bg_color(hvacElements.coolButton, lv_color_hex(0x333333), 0);
         } else if (hvacState.mode == "cool") {
-            lv_obj_set_style_bg_color(hvacElements.offButton, lv_color_hex(COLOR_SURFACE), 0);
-            lv_obj_set_style_bg_color(hvacElements.coolButton, lv_color_hex(COLOR_SUCCESS), 0);
+            lv_obj_set_style_bg_color(hvacElements.offButton, lv_color_hex(0x333333), 0);
+            lv_obj_set_style_bg_color(hvacElements.coolButton, lv_color_hex(0x2196F3), 0);
         } else {
-            lv_obj_set_style_bg_color(hvacElements.offButton, lv_color_hex(COLOR_SUCCESS), 0);
-            lv_obj_set_style_bg_color(hvacElements.coolButton, lv_color_hex(COLOR_SURFACE), 0);
+            lv_obj_set_style_bg_color(hvacElements.offButton, lv_color_hex(0x333333), 0);
+            lv_obj_set_style_bg_color(hvacElements.coolButton, lv_color_hex(0x333333), 0);
         }
         
-        // Update target temperature - just the number, no "°C"
-        char targetTempStr[5];
-        snprintf(targetTempStr, sizeof(targetTempStr), "%.0f", hvacState.targetTemp);
-        lv_label_set_text(hvacElements.targetTempValueLabel, targetTempStr);
+        // ASCII art temperature is static for now - would need complex redrawing for dynamic updates
+        Serial.print("Temperature: ");
+        Serial.println(hvacState.targetTemp);
     }
 }
 
@@ -277,7 +270,45 @@ void ScreenManager::update() {
 }
 
 void ScreenManager::lightPowerButtonEvent(lv_event_t* e) {
-    mqttHandler.setLightState(!lightState.isOn);
+    // No power button in the new design
+}
+
+void ScreenManager::brightnessBarEvent(lv_event_t* e) {
+    lv_obj_t* bar = lv_event_get_target(e);
+    lv_point_t point;
+    lv_indev_get_point(lv_indev_get_act(), &point);
+    
+    lv_coord_t bar_x = lv_obj_get_x(bar);
+    lv_coord_t bar_width = lv_obj_get_width(bar);
+    lv_coord_t touch_x = point.x - bar_x;
+    
+    if (touch_x >= 0 && touch_x <= bar_width) {
+        int new_brightness = MIN_BRIGHTNESS + (touch_x * (MAX_BRIGHTNESS - MIN_BRIGHTNESS)) / bar_width;
+        if (new_brightness < MIN_BRIGHTNESS) new_brightness = MIN_BRIGHTNESS;
+        if (new_brightness > MAX_BRIGHTNESS) new_brightness = MAX_BRIGHTNESS;
+        
+        mqttHandler.setLightBrightness(new_brightness);
+        lv_bar_set_value(bar, new_brightness, LV_ANIM_OFF);
+    }
+}
+
+void ScreenManager::colorTempBarEvent(lv_event_t* e) {
+    lv_obj_t* bar = lv_event_get_target(e);
+    lv_point_t point;
+    lv_indev_get_point(lv_indev_get_act(), &point);
+    
+    lv_coord_t bar_x = lv_obj_get_x(bar);
+    lv_coord_t bar_width = lv_obj_get_width(bar);
+    lv_coord_t touch_x = point.x - bar_x;
+    
+    if (touch_x >= 0 && touch_x <= bar_width) {
+        int new_color_temp = MIN_COLOR_TEMP + (touch_x * (MAX_COLOR_TEMP - MIN_COLOR_TEMP)) / bar_width;
+        if (new_color_temp < MIN_COLOR_TEMP) new_color_temp = MIN_COLOR_TEMP;
+        if (new_color_temp > MAX_COLOR_TEMP) new_color_temp = MAX_COLOR_TEMP;
+        
+        mqttHandler.setLightColorTemp(new_color_temp);
+        lv_bar_set_value(bar, new_color_temp, LV_ANIM_OFF);
+    }
 }
 
 void ScreenManager::hvacOffButtonEvent(lv_event_t* e) {
@@ -293,9 +324,9 @@ void ScreenManager::hvacTempUpButtonEvent(lv_event_t* e) {
     if (newTemp <= MAX_TEMPERATURE) {
         mqttHandler.setHVACTemperature(newTemp);
         
-        char tempStr[5];
-        snprintf(tempStr, sizeof(tempStr), "%.0f", newTemp);
-        lv_label_set_text(instance->hvacElements.targetTempValueLabel, tempStr);
+        // For now just print to console - will need to redraw ASCII art
+        Serial.print("New temp: ");
+        Serial.println((int)newTemp);
     }
 }
 
@@ -304,8 +335,8 @@ void ScreenManager::hvacTempDownButtonEvent(lv_event_t* e) {
     if (newTemp >= MIN_TEMPERATURE) {
         mqttHandler.setHVACTemperature(newTemp);
         
-        char tempStr[5];
-        snprintf(tempStr, sizeof(tempStr), "%.0f", newTemp);
-        lv_label_set_text(instance->hvacElements.targetTempValueLabel, tempStr);
+        // For now just print to console - will need to redraw ASCII art
+        Serial.print("New temp: ");
+        Serial.println((int)newTemp);
     }
 }
